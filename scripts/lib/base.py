1935d099b71378f919142faf5cd611a6657ac79d
Geoffrey Bauduin <bauduin.geo@gmail.com>
2021-09-13 00:42:15 +0200

    fix: druids and priests can use 'perle d'ombre purifiée'

--- scripts/lib/base.py
+++ scripts/lib/base.py
@@ -7,6 +7,9 @@ class Base(object):
         if hasattr(self, "restricted_fights"):
             if fight.name not in self.restricted_fights:
                 return True
+        if hasattr(self, "restricted_classes"):
+            if player.subType not in self.restricted_classes:
+                return True
         if hasattr(self, "invalid") and self.invalid:
             return True
         return False

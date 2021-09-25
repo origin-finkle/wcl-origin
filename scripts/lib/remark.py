55b5da0399c2d3df5eace2057c66fb48c5e5f0ad
Geoffrey Bauduin <bauduin.geo@gmail.com>
2021-09-25 21:26:26 +0200

    perf(build): enhance build time

--- scripts/lib/remark.py
+++ scripts/lib/remark.py
@@ -1,5 +1,12 @@
+from hashlib import sha256
 from .base import Base
 
 
 class Remark(Base):
-    pass
+    def __init__(self, data):
+        super().__init__(data)
+        uuid = [self.type]
+        for attr in ("wowhead_attr", "item_wowhead_attr", "spell_wowhead_attr"):
+            if hasattr(self, attr) and (v := getattr(self, attr)):
+                uuid.append(v)
+        self.uuid = ":".join(uuid)
